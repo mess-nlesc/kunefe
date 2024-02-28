64325670eb9f7eba226c16427ce309be328f3266
Faruk D. <f.diblen@esciencecenter.nl>
2024-02-28 15:46:36 +0100

    update run_command_on_remote example

--- examples/run_command_on_remote.py
+++ examples/run_command_on_remote.py
@@ -3,6 +3,9 @@ from kunefe import Kunefe
 # initialize kunefe by providing connection details
 kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
 
+# set up clients and connect to the remote
+kunefe.connect_remote()
+
 # run 'ls' command on the remote system
 kunefe.run_remote_command(command='ls /home/xenon')
 

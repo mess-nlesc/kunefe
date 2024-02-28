7be525405baa5f7c62377009e1e2e82453046483
Faruk D. <f.diblen@esciencecenter.nl>
2024-02-28 12:47:26 +0100

    start the Docker container before testing the connection and remove after

--- tests/test_kunefe.py
+++ tests/test_kunefe.py
@@ -1,8 +1,44 @@
 """Tests for the kunefe"""
+from time import sleep
+import pytest
+import docker
 from kunefe import Kunefe
 
+
+# To see the print messages run with '--capture=no':
+# pytest --capture=no -v tests/test_kunefe.py
+@pytest.fixture(scope='session', autouse=True)
+def slurm_service():
+    """Start the SLURM service before running the tests
+    """
+    container_name = "test_kunefe"
+    docker_image_name = "xenonmiddleware/slurm:latest"
+
+    # https://docker-py.readthedocs.io/en/stable/containers.html
+
+    # start the SLURM service before tests
+    client = docker.from_env()
+    print('\nStarting the Docker container')
+    container = client.containers.run(
+        name=container_name,
+        image=docker_image_name,
+        detach=True,
+        ports={'22/tcp': 10022}
+    )
+    sleep(10)
+    container_state = container.attrs['State']
+    assert container_state['Status'] == 'created'
+
+    yield container
+
+    # stop the SLURM service after the last test
+    print('\nStopping the Docker container')
+    container.stop()
+    container.remove(force=True)
+
+
 # https://docs.pytest.org/en/latest/how-to/monkeypatch.html
-def test_connection_with_password(monkeypatch):
+def test_connection_with_password(monkeypatch, slurm_service):
     """Test if kunefe can ssh to the SLURM Docker container using a password
     """
     responses = iter(['javagat'])  # the default password

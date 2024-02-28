1279cbb8d0fac63ea2e7f0b6ba253c19cbd7491c
Faruk D. <f.diblen@esciencecenter.nl>
2024-02-28 13:23:00 +0100

    add tests for check_required_tools method

--- tests/test_kunefe.py
+++ tests/test_kunefe.py
@@ -46,61 +46,79 @@ def test_connection_with_password(monkeypatch, slurm_service):
     # assert Kunefe(username="xenon", hostname="localhost", port=10022) == ("Me", "xxx")
 
 
-def test_create_remote_folder():
-    """Test create_remote_folder
+# def test_create_remote_folder():
+#     """Test create_remote_folder
+#     """
+#     pass
+
+
+# def test_get_files():
+#     """Test get_files
+#     """
+#     pass
+
+
+# def test_put_files():
+#     """Test put_files
+#     """
+#     pass
+
+
+# def test_submit_job():
+#     """Test submit_job
+#     """
+#     pass
+
+
+# def test_build_apptainer_image():
+#     """Test build_apptainer_image
+#     """
+#     pass
+
+
+# def test_check_local_command_exists():
+#     """Test check_local_command_exists
+#     """
+#     pass
+
+
+def test_check_required_tools(monkeypatch):
+    """Test if required tools can be found
     """
-    pass
+    responses = iter(['javagat'])  # the default password
+    monkeypatch.setattr('getpass.getpass', lambda _: next(responses))
+
+    kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
+    requirements_status = kunefe.check_required_tools(['docker', 'apptainer'])
+    print(f'Requirements status: {requirements_status}')
+    assert requirements_status is True, 'all the required tools need to be installed on your system'
 
 
-def test_get_files():
-    """Test get_files
+def test_check_required_tools_fail(monkeypatch):
+    """Test if check_required_tools fails for a command that does not exist
     """
-    pass
+    responses = iter(['javagat'])  # the default password
+    monkeypatch.setattr('getpass.getpass', lambda _: next(responses))
+
+    kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
+    requirements_status = kunefe.check_required_tools(['docker', 'apptainer', 'bsxcommand'])
+    print(f'Requirements status: {requirements_status}')
+    assert requirements_status is False, 'should fail for bsxcommand'
 
 
-def test_put_files():
-    """Test put_files
-    """
-    pass
+# def test_generate_job_file():
+#     """Test generate_job_file
+#     """
+#     pass
 
 
-def test_submit_job():
-    """Test submit_job
-    """
-    pass
+# def test_run_remote_command():
+#     """Test run_remote_command
+#     """
+#     pass
 
 
-def test_build_apptainer_image():
-    """Test build_apptainer_image
-    """
-    pass
-
-
-def test_check_local_command_exists():
-    """Test check_local_command_exists
-    """
-    pass
-
-
-def test_check_required_tools():
-    """Test check_required_tools
-    """
-    pass
-
-
-def test_generate_job_file():
-    """Test generate_job_file
-    """
-    pass
-
-
-def test_run_remote_command():
-    """Test run_remote_command
-    """
-    pass
-
-
-def test_watch_slurm_queue():
-    """Test watch_slurm_queue
-    """
-    pass
+# def test_watch_slurm_queue():
+#     """Test watch_slurm_queue
+#     """
+#     pass

0c073fafca751241562ad36a9b123eafa0db66f4
Faruk D. <f.diblen@esciencecenter.nl>
2024-02-28 11:46:09 +0100

    fix kunefe import in __init__.py

--- kunefe/__init__.py
+++ kunefe/__init__.py
@@ -1,8 +1,13 @@
 """Documentation about kunefe."""
 import logging
+from .kunefe import Kunefe
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Faruk D."
 __email__ = "f.diblen@esciencecenter.nl"
 __version__ = "0.1.0"
+
+__all__ = [
+    "Kunefe"
+]

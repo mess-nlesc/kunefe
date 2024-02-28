9bea966ef44c2f47ce70d308dde5465cdb2e2895
Faruk D. <f.diblen@esciencecenter.nl>
2024-02-28 15:22:29 +0100

    update the main example

--- examples/use_kunefe.py
+++ examples/use_kunefe.py
@@ -3,45 +3,43 @@
 
 from kunefe import Kunefe
 
-if __name__ == "__main__":
+kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
 
-    kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
+# # submit a new job
+# job_id, stdin, stdout, stderr = kunefe.submit_job(
+#     job_file="/home/xenon/test-slurm.job"
+# )
+# print(f'job_id: {job_id}')
 
-    # # submit a new job
-    # job_id, stdin, stdout, stderr = kunefe.submit_job(
-    #     job_file="/home/xenon/test-slurm.job"
-    # )
-    # print(f'job_id: {job_id}')
+# # copy files to the server
+# kunefe.put_files(
+#     remote_folder="/home/xenon/",
+#     local_folder="./test_folder"
+# )
 
-    # # copy files to the server
-    # kunefe.put_files(
-    #     remote_folder="/home/xenon/",
-    #     local_folder="./test_folder"
-    # )
+# # download files from the server
+# kunefe.get_files(
+#     remote_folder="/home/xenon/test_folder",
+#     local_folder="./copy_of_test_folder"
+# )
 
-    # # download files from the server
-    # kunefe.get_files(
-    #     remote_folder="/home/xenon/test_folder",
-    #     local_folder="./copy_of_test_folder"
-    # )
+# # check if the required tools (commands) available on the host system
+# requirements_status = kunefe.check_required_tools(['docker', 'apptainer'])
+# print(f'Requirements status: {requirements_status}')
 
-    # # check if the required tools (commands) available on the host system
-    # requirements_status = kunefe.check_required_tools(['docker', 'apptainer'])
-    # print(f'Requirements status: {requirements_status}')
+# # build the apptainer image from the docker image
+# image_build_status = kunefe.build_apptainer_image(netlogo_version='6.3.0')
+# print(f'Image build status: {image_build_status}')
 
-    # # build the apptainer image from the docker image
-    # image_build_status = kunefe.build_apptainer_image(netlogo_version='6.3.0')
-    # print(f'Image build status: {image_build_status}')
+# # generate a slurm job file
+# kunefe.generate_job_file(
+#     job_name='kunefe_experiment_job',
+#     model_path="/home/model/path",
+#     sif_file_path="/home/xenon/netlogo_6.3.0.sif",
+#     experiment_name="test_experiment",
+#     table_name="table_name",
+#     job_time='0:30:00',
+# )
 
-    # # generate a slurm job file
-    # kunefe.generate_job_file(
-    #     job_name='kunefe_experiment_job',
-    #     model_path="/home/model/path",
-    #     sif_file_path="/home/xenon/netlogo_6.3.0.sif",
-    #     experiment_name="test_experiment",
-    #     table_name="table_name",
-    #     job_time='0:30:00',
-    # )
-
-    # # watch the slurm queue
-    # kunefe.watch_slurm_queue()
+# # watch the slurm queue
+# kunefe.watch_slurm_queue()

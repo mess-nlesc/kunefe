8bff1d890069bf8d0013e2ef66d1006a0ce88e56
Faruk D. <f.diblen@esciencecenter.nl>
2024-03-11 12:45:04 +0100

    netlogo job template is not needed anymore

--- examples/generate_generic_job_script.py
+++ examples/generate_generic_job_script.py
@@ -5,7 +5,7 @@ kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
 
 # generate a slurm job script
 kunefe.generate_job_script(
-    job_name='kunefe_experiment_job',
+    job_name='kunefe_generic_job',
     sif_file_path="/home/xenon/myapp_0.1.0.sif",
     command="ls /home/xenon",
     env_vars="PATH=$PATH:/home/xenon",

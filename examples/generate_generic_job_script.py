d09cfb341222ddf6f5ca09758257c8270fbb437b
Faruk D. <f.diblen@esciencecenter.nl>
2024-03-11 12:30:45 +0100

    add example code to generate a job script

---
+++ examples/generate_generic_job_script.py
@@ -0,0 +1,13 @@
+from kunefe import Kunefe
+
+# initialize kunefe by providing connection details
+kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
+
+# generate a slurm job script
+kunefe.generate_job_script(
+    job_name='kunefe_experiment_job',
+    sif_file_path="/home/xenon/myapp_0.1.0.sif",
+    command="ls /home/xenon",
+    env_vars="PATH=$PATH:/home/xenon",
+    job_time='0:30:00',
+)

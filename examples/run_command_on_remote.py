41007c1a025aa12098a722aa9c1068e57242f1ea
Faruk D. <f.diblen@esciencecenter.nl>
2024-03-11 12:59:15 +0100

    update the remote command example

--- examples/run_command_on_remote.py
+++ examples/run_command_on_remote.py
@@ -6,8 +6,11 @@ kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
 # set up clients and connect to the remote
 kunefe.connect_remote()
 
-# run 'ls' command on the remote system
-kunefe.run_remote_command(command='ls /home/xenon')
+# run 'ls' command on the remote system and show the output
+stdin, stdout, stderr = kunefe.run_remote_command(command='ls /home/xenon')
+print(f'stdin:\n{stdin}')
+print(f'stdout:\n{stdout}')
+print(f'stderr:\n{stderr}')
 
 # # run 'ls'but clean the output
 # kunefe.run_remote_command(command='ls -la', flush=True)

948feaf6d662b5eeab9fb177138e087ba40fc65d
Faruk D. <f.diblen@esciencecenter.nl>
2024-03-12 11:24:52 +0100

    split remote and local tests

--- tests/test_kunefe_on_remote.py
+++ tests/test_kunefe_on_remote.py
@@ -1,4 +1,4 @@
-"""Tests for the kunefe."""
+"""Tests remote functions of kunefe ."""
 from time import sleep
 import docker
 import pytest
@@ -67,44 +67,6 @@ def test_connection_with_password(monkeypatch, slurm_service):
 
 # def test_submit_job():
 #     """Test submit_job
-#     """
-#     pass
-
-
-# def test_build_apptainer_image():
-#     """Test build_apptainer_image
-#     """
-#     pass
-
-
-# def test_check_local_command_exists():
-#     """Test check_local_command_exists
-#     """
-#     pass
-
-
-# to only test this function run:
-# pytest --capture=no -v tests/test_kunefe.py::test_check_required_tools
-def test_check_required_tools():
-    """Test if required tools can be found."""
-    kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
-    requirements_status = kunefe.check_required_tools(['docker', 'rsync'])
-    print(f'Requirements status: {requirements_status}')
-    assert requirements_status is True, 'all the required tools need to be installed on your system'
-
-
-# to only test this function run:
-# pytest --capture=no -v tests/test_kunefe.py::test_check_required_tools_fail
-def test_check_required_tools_fail():
-    """Test if check_required_tools fails for a command that does not exist."""
-    kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
-    requirements_status = kunefe.check_required_tools(['docker', 'rsync', 'bsxcommand'])
-    print(f'Requirements status: {requirements_status}')
-    assert requirements_status is False, 'should fail for bsxcommand'
-
-
-# def test_generate_job_script():
-#     """Test generate_job_script
 #     """
 #     pass
 

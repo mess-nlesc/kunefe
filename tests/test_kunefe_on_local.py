9663b066a95dbfdec6c4de7e8176bf02f602acd1
Faruk D. <f.diblen@esciencecenter.nl>
2024-03-12 11:36:12 +0100

    add a placeholder test for checking generated apptainer image

--- tests/test_kunefe_on_local.py
+++ tests/test_kunefe_on_local.py
@@ -22,6 +22,13 @@ def test_build_apptainer_image_fail(tmp_path):
     )
 
 
+# # See https://stackoverflow.com/a/52877169
+# def test_compare_generated_apptainer_image():
+#     """Test build_apptainer_image
+#     """
+#     pass
+
+
 def test_check_local_command_exists():
     """Test check_local_command_exists."""
     kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)

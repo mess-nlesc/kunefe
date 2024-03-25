e7933601b5fc7755fdefee373c1ba50226966bee
Faruk D. <f.diblen@esciencecenter.nl>
2024-03-25 16:29:57 +0100

    add an example to show how to install apptainer on remote

--- examples/install_apptainer_on_remote.py
+++ examples/install_apptainer_on_remote.py
@@ -6,4 +6,4 @@ kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
 # set up clients and connect to the remote
 kunefe.connect_remote()
 
-kunefe.install_apptainer_on_remote(install_path="/home/xenon/tools/")
+kunefe.install_apptainer_on_remote(install_path="/home/xenon/tools")

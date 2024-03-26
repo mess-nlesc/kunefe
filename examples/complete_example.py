27ee9812d4ad5df3e631225b2093df6c7ae0676f
Faruk D. <f.diblen@esciencecenter.nl>
2024-03-26 13:37:42 +0100

    improve the complete NetLogo example

--- examples/complete_example.py
+++ examples/complete_example.py
@@ -1,78 +1,71 @@
-"""A complete example showing how to test kunefe module."""
+"""An example showing how to run containerized NetLogo on HPC system."""
 
 import os
 from kunefe import Kunefe
 
-# set up a local slurm cluster
-# docker pull xenonmiddleware/slurm:latest
-# docker run --detach --publish 10022:22 xenonmiddleware/slurm:latest
+# define the constants
+OUTPUT_FOLDER_LOCAL = "./output/"    # local folder to save output
+REMOTE_HOME = "/home/olyashevska/"   # home folder on the remote system
+OUTPUT_FOLDER_REMOTE = REMOTE_HOME + "test_folder/"
+# specify the Docker image
+NETLOGO_VERSION = "6.3.0"
+DOCKER_IMAGE = f"comses/netlogo:{NETLOGO_VERSION}"
+# specify the apptainer image
+SIF_FILE_NAME = "netlogo_6.3.0.sif"
+SIF_FILE_PATH_LOCAL = OUTPUT_FOLDER_LOCAL + SIF_FILE_NAME
+SIF_FILE_PATH_REMOTE = OUTPUT_FOLDER_REMOTE + SIF_FILE_NAME
 
-## generate an apptainer image
-# initialize kunefe by providing connection details
-
-# run using xenon
-# kunefe = Kunefe(username="xenon", hostname="localhost", port=10022)
-# home_folder = "/home/xenon/"
-
-# run on snellius
+# We will run the NetLogo simulation on Snellius cluster:
+# https://www.surf.nl/diensten/snellius-de-nationale-supercomputer
 kunefe = Kunefe(username="olyashevska", hostname="snellius.surf.nl", port=22)
-home_folder = "/home/olyashevska/"
 
-# create test folder
-local_folder = "./test_folder/"
+# create a local folder to save the generated files and the output
 try:
-    os.mkdir(local_folder)
-    print(f"Directory '{local_folder}' created.")
+    os.mkdir(OUTPUT_FOLDER_LOCAL)
+    print(f"Directory '{OUTPUT_FOLDER_LOCAL}' created.")
 except FileExistsError:
-    print(f"Directory '{local_folder}' already exists.")
-
-# specify the Docker image to use
-version = "6.3.0"
-docker_image = f"comses/netlogo:{version}"
-sif_file_name = "netlogo_6.3.0.sif"
-local_sif_file_path = local_folder + sif_file_name
-
-remote_folder = home_folder + "test_folder/"
-remote_sif_file_path = remote_folder + sif_file_name
+    print(f"Directory '{OUTPUT_FOLDER_LOCAL}' already exists.")
 
 # build apptainer image from a Docker image
 kunefe.build_apptainer_image(
-    docker_image=docker_image, sif_file_name=local_sif_file_path
+    docker_image=DOCKER_IMAGE, sif_file_name=SIF_FILE_PATH_LOCAL
 )
 
+# make a connection to the remote
 kunefe.connect_remote()
 
-kunefe.create_remote_folder(remote_folder=remote_folder)
+# create a folder which the files will be copied to
+kunefe.create_remote_folder(remote_folder=OUTPUT_FOLDER_REMOTE)
 
-## generate a job script
-netlogo_command = """/opt/netlogo/netlogo-headless.sh \
+# define the job name and the command to be used to run NetLogo
+JOB_NAME = "kunefe_netlogo_experiment_job"
+NETLOGO_COMMAND = """/opt/netlogo/netlogo-headless.sh \
 --model 'model_path' \
 --experiment 'experiment_name' \
 --table 'table_name'
 """
 
+# generate a job script to be submitted to HPC job queue
 kunefe.generate_job_script(
-    job_name="kunefe_netlogo_experiment_job",
-    sif_file_path=remote_sif_file_path,
-    command=netlogo_command,
+    job_name=JOB_NAME,
+    sif_file_path=SIF_FILE_PATH_REMOTE,
+    command=NETLOGO_COMMAND,
     env_vars="JAVA_TOOL_OPTIONS=-Xmx8G",
-    job_file_path=local_folder,
+    job_file_path=OUTPUT_FOLDER_LOCAL,
     job_time="0:1:00",
     template_name="generic",
 )
 
-## copy the required files and the job script to the remote system
+# copy the required files and the job script to the remote system
+kunefe.put_files(remote_folder=OUTPUT_FOLDER_REMOTE, local_folder=OUTPUT_FOLDER_LOCAL, verbose=True)
 
-kunefe.put_files(remote_folder=remote_folder, local_folder=local_folder, verbose=True)
-
-## submit the job
-# job_id, stdin, stdout, stderr = kunefe.submit_job(job_file="/home/xenon/test_folder/kunefe_netlogo_experiment_job.sh")
+# submit the job
 job_id, stdin, stdout, stderr = kunefe.submit_job(
-    job_file="/home/olyashevska/test_folder/kunefe_netlogo_experiment_job.sh"
+    job_file=f"{OUTPUT_FOLDER_REMOTE}/{JOB_NAME}.sh"
 )
 
-## monitor the queue
+# monitor the queue
 kunefe.watch_slurm_queue()
 
-## retrive the job results
-# kunefe.get_files(remote_folder=remote_folder, local_folder="./copy_of_test_folder")
+# retrive the job results
+kunefe.get_files(remote_folder=OUTPUT_FOLDER_REMOTE, local_folder=f"{OUTPUT_FOLDER_LOCAL}/result")
